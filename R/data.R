030dcc5db4407f694bfef9a3cad314085bfecb6e
Kyuri <kyurheep@gmail.com>
2022-11-25 11:11:35 +0100

    edit tutorial/added Birthweight data

--- R/data.R
+++ R/data.R
@@ -2,7 +2,45 @@
 #'
 #' This data was pre-processed for this package specifically.
 #'
+#' \describe{
+#'   \item{EXT}{Extraversion}
+#'   \item{EST}{Emotional Stability}
+#'   \item{AGR}{Agreeableness}
+#'   \item{CSN}{Conscientiousness}
+#'   \item{OPN}{Openness to new experiences}
+#' }
 #' @seealso [synp_get_param()]
 #'
 #' @source <https://www.kaggle.com/datasets/tunguz/big-five-personality-test>
+#'
+#' Check the codebook <https://www.kaggle.com/datasets/tunguz/big-five-personality-test> if you want to know each of the items in detail.
 "big5"
+
+
+
+
+
+#' Example Birthweight data
+#'
+#' This dataset contains information on new born babies and their parents.
+#'
+#' \describe{
+#'   \item{ID}{Baby number}
+#'   \item{length}{Length of baby (cm)}
+#'   \item{Birthweight}{Weight of baby (kg)}
+#'   \item{headcirumference}{Head Circumference}
+#'   \item{Gestation}{Gestation (weeks)}
+#'   \item{smoker}{Mother smokes 1 = smoker 0 = non-smoker}
+#'   \item{motherage}{Maternal age}
+#'   \item{mnocig}{Number of cigarettes smoked per day by mother}
+#'   \item{mheight}{Mothers height (cm)}
+#'   \item{mppwt}{Mothers pre-pregnancy weight (kg)}
+#'   \item{fage}{Father's age}
+#'   \item{fedyrs}{Father’s years in education}
+#'   \item{fnocig}{Number of cigarettes smoked per day by father}
+#'   \item{fweight}{Father's weight (kg)}
+#'   \item{lowbwt}{Low birth weight, 0 = No and 1 = yes (cut off 2.6kg)}
+#'   \item{mage35}{Mother over 35, 0 = No and 1 = yes}
+#' }
+#' @source <https://www.sheffield.ac.uk/mash/statistics/datasets>
+"Birthweight"
